4fdea3c79e34ae8a4d98eace307530c611fe291f
ningyunhan <ningyunhan19940829@gmail.com>
2018-02-02 14:31:05 -0500

    nightmare

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
 
-  while(balls.length < 25) {
+  while(balls.length < 200) {
     var ball = new Ball(
       random(0,width),
       random(0,height),
